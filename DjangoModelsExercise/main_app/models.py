53dd3981b70cfd6300a83cdd83768d898dbfb21e
TsvetkoPetkov23 <115669197+TsvetkoPetkov@users.noreply.github.com>
2023-11-07 10:52:10 +0200

    Update models.py

--- DjangoModelsExercise/main_app/models.py
+++ DjangoModelsExercise/main_app/models.py
@@ -6,13 +6,11 @@ class Person(models.Model):
     name = models.CharField(
         max_length=30,
     )
-
     age = models.PositiveIntegerField()
 
 
 class Blog(models.Model):
     post = models.TextField()
-
     author = models.CharField(
         max_length=35,
     )
